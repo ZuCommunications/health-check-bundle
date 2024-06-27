025ab9fe507bd24c7972e0b5a38a31ec9fed819e
MaximilianGewers <maximilian.gewers@gmail.com>
2024-06-27 14:08:09 -0600

    chore: hooking up config

--- src/ZuHealthCheckBundle.php
+++ src/ZuHealthCheckBundle.php
@@ -4,17 +4,12 @@ namespace Zu\HealthCheckBundle;
 
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
-use Zu\HealthCheckBundle\Controller\HealthCheckController;
 use Zu\HealthCheckBundle\Services\DoctrineCheckService;
-use Zu\HealthCheckBundle\Services\HealthCheckService;
 
 class ZuHealthCheckBundle extends AbstractBundle
 {
-    protected $name = 'ZuHealthCheckBundle';
-
     public function configure(DefinitionConfigurator $definition): void
     {
         $definition->rootNode()
@@ -31,5 +26,11 @@ class ZuHealthCheckBundle extends AbstractBundle
 
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
+        $container->import('../config/services.yaml');
+
+        // This is not working. Has no effect.
+        $container->services()->set('Zu\HealthCheckBundle\Services\DoctrineCheckService')
+            ->arg(0, true)
+        ;
     }
 }
